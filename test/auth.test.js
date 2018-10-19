0a01544b679a1273c2b0f843ebfbbfc702f3a13b
Johan Codinha <johan@codinha.net>
2018-10-19 23:11:07 +1100

    use tape instead of ava because it is simplier and doesn't need it's own runner

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,31 +1,29 @@
-import test from 'ava'
-import request from 'supertest'
-import app from '../main'
-import { username, password } from '../config/test.js'
-
-test.beforeEach(async t => {
-  t.context.request = request(app)
-})
+const test = require('tape')
+const request = require('supertest')
+const app = require('../main')
+const { username, password } = require('../config/test.js')
 
 test('Auth - guest login return jwt', async t => {
-  const {body, status, type} = await t.context.request.get('/auth/guest')
+  const {body, status, type} = await request(app).get('/auth/guest')
   t.is(status, 200)
   t.is(typeof body.jwt, 'string')
   t.is(type, 'application/json')
+  t.end()
 })
 
 test('Auth - user login return jwt', async t => {
-  const { body, status, type } = await t.context.request
+  const { body, status, type } = await request(app)
     .post('/auth')
     .send({ username, password })
   t.is(status, 200)
   t.is(typeof body.jwt, 'string')
   t.is(typeof body.displayName, 'string')
   t.is(type, 'application/json')
+  t.end()
 })
 
 test('Auth - user login return error with missing password', async t => {
-  const { body, status, type } = await t.context.request
+  const { body, status, type } = await request(app)
     .post('/auth')
     .send({ username: 'Karen' })
   t.is(status, 400)
@@ -33,10 +31,11 @@ test('Auth - user login return error with missing password', async t => {
   t.is(Array.isArray(body.errors), true)
   t.is(!!body.errors.find(e => e.message === 'Missing required parameter: password'), true)
   t.is(body.errors.length, 1)
+  t.end()
 })
 
 test('Auth - user login return error with missing username', async t => {
-  const { body, status, type } = await t.context.request
+  const { body, status, type } = await request(app)
     .post('/auth')
     .send({ password: 'neccesary' })
   t.is(status, 400)
@@ -44,13 +43,15 @@ test('Auth - user login return error with missing username', async t => {
   t.is(Array.isArray(body.errors), true)
   t.is(!!body.errors.find(e => e.message === 'Missing required parameter: username'), true)
   t.is(body.errors.length, 1)
+  t.end()
 })
 
 test('Auth - user login return error with missing credentials', async t => {
-  const { body, status, type } = await t.context.request
+  const { body, status, type } = await request(app)
     .post('/auth')
     .send({})
   t.is(status, 400)
   t.is(type, 'application/json')
   t.is(body.errors.length, 2)
+  t.end()
 })
